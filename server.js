0175fa24250923b41fe5f63b3d9d42b71dade1cd
Romit Singh Bhau <romitenvious@icloud.com>
2019-06-23 14:14:44 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,22 +7,46 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-  
+createPool
   
 // default route
 app.get('/', function (req, res) {
     return res.send({ error: true, message: 'hello' })
 });
 // connection configurations
-var dbConn = mysql.createConnection({
+var dbConn = {
     host: 'localhost',
     user: 'root',
-    password: 'root',
-    database: 'cp'
-});
+    password: 'anticounterfeit',
+    database: 'abbyfy'
+  };
   
+  var connection;
+  
+  function handleDisconnect() {
+    connection = mysql.createConnection(dbConn); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+  
+    connection.connect(function(err) {              // The server is either down
+      if(err) {                                     // or restarting (takes a while sometimes).
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
+  }
+  
+  handleDisconnect();
 // connect to database
-dbConn.connect(); 
+
  
 app.use(function (req, res, next) {
 
@@ -49,28 +73,71 @@ app.post('/login', function (req, res) {
     var email = req.body.email;
     var password = req.body.password;
     var category = req.body.category;
-    dbConn.query("SELECT category FROM login where email = ? AND password = ? AND category = ?", [email, password, category], function (error, results, fields) {
-        if (error) throw error;
+    connection.query("SELECT category FROM users where email = ? AND password = ? AND category = ?", [email, password, category], function (error, results, fields) {
         return res.send({ error: false, output: results, message: 'success' });
     });
 });
 
+// Profile details
+app.post('/profile', function (req, res) {
+    
+    var email = req.body.email;
+    connection.query("SELECT * FROM users where email = ?", email , function (error, results, fields) {
+        return res.send({ error: false, output: results, message: 'success' });
+    });
+});
+
+// all emails
+app.post('/emails', function (req, res) {
+    
+    var email = req.body.email;
+    connection.query("SELECT email FROM users ", function (error, results, fields) {
+        return res.send({ error: false, output: results, message: 'success' });
+    });
+});
+
+// View Messages
+app.post('/mymessages', function (req, res) {
+    
+    var email = req.body.email;
+    connection.query("SELECT * FROM Messages where user_id_from = ? or user_id_to = ?", [email,email] , function (error, results, fields) {
+        return res.send({ error: false, output: results, message: 'success' });
+    });
+});
+
+// Send Messages
+app.post('/sendmessages', function (req, res) {
+    
+    var from = req.body.from;
+    var to = req.body.to;
+    var content = req.body.content;
+    var date = req.body.date;
+    connection.query("INSERT INTO Messages ( message_id, user_id_from, user_id_to, content, date_created ) VALUES  ( null, ? , ? , ? , ? )", [from,to,content,date] , function (error, results, fields) {
+        return res.send({ error: false, output: results, message: 'success' });
+    });
+});
+
+
 // Signup
 app.post('/signup', function (req, res) {
     
+    var first_name = req.body.first_name;
+    var last_name = req.body.last_name;
+    var address = req.body.address;
     var email = req.body.email;
     var password = req.body.password;
-    dbConn.query("SELECT category FROM login where email = ? AND password = ?", [email, password], function (error, results, fields) {
-        if (error) throw error;
-        return res.send({ error: false, data: results, message: 'users list.' });
+    var category = req.body.category;
+    connection.query("INSERT INTO users ( id, first_name, last_name, address, email, password, category ) VALUES  ( null, ? , ? , ? , ? , ? , ?)", [first_name,last_name,address,email,password,category] , function (error, results, fields) {
+        return res.send({ error: false, output: results, message: 'success' });
     });
 });
+
  
  
  
 // set port
-app.listen(3000, function () {
-    console.log('Node app is running on port 3000');
+app.listen(8080, function () {
+    console.log('Node app is running on port 8080');
 });
  
 module.exports = app;
